e566001d536f22460892f9b4b28ae7ba5675527a
Mubasshir-ali <mubasshir.ali42@gmail.com>
2021-04-29 12:28:02 +0500

    create dashboard.html file and this settings

--- AI_ENGINEER/app.py
+++ AI_ENGINEER/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, session, redirect
 import pymongo
 from pymongo import MongoClient
+from functools import wraps
+
+# create routes
+from user import routes
 
 # cluster = MongoClient("mongodb+srv://aieng:<Pagalalone42!>@cluster0.s23zp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 # db = cluster("aieng")
 # collection = ("aieng")
 
-# create routes
-from user import routes
+
 
 
 # Create instance of Flask model is app
@@ -17,13 +20,30 @@ app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
 # Database connection
 client = pymongo.MongoClient('localhost', 27017)
-db = client.user_login_system
+db = client.aieng
+
+# Decorators
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+
+    return wrap
 
 
 # create decorator is @app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 
 @app.route("/about")
@@ -73,6 +93,47 @@ def forgot_password():
 @app.route("/page-not-found")
 def page_not_found():
     return render_template("404.html")
+
+@app.route("/blank")
+def blank():
+    return render_template("blank.html")
+
+@app.route("/buttons")
+def buttons():
+    return render_template("buttons.html")
+
+@app.route("/cards")
+def cards():
+    return render_template("cards.html")
+
+@app.route("/charts")
+def charts():
+    return render_template("charts.html")
+
+@app.route("/tables")
+def tables():
+    return render_template("tables.html")
+
+@app.route("/animation")
+def animation():
+    return render_template("animation.html")
+
+@app.route("/border")
+def border():
+    return render_template("border.html")
+
+@app.route("/color")
+def color():
+    return render_template("color.html")
+
+@app.route("/other")
+def other():
+    return render_template("other.html")
+
+
+
+
+
 
 
 # Debugger Mode is ON when we check any errors in runtime And when the site is complete then the Debugger Mode is OFF
